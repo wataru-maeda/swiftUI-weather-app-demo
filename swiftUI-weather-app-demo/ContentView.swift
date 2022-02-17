889e3b09f6b7bca170e93d20095143b6df0e6a74
WataruMaeda <w.maeda.jp@gmail.com>
2022-02-16 21:45:07 -0800

    chore: update

--- swiftUI-weather-app-demo/ContentView.swift
+++ swiftUI-weather-app-demo/ContentView.swift
@@ -11,17 +11,30 @@ struct ContentView: View {
   init() {
     UINavigationBar.appearance().titleTextAttributes = [.font: UIFont(name: "pilgi", size: 24)!, .foregroundColor: #colorLiteral(red: 0.9449525476, green: 0.2824411094, blue: 0.4755416512, alpha: 1)]
   }
-
+  
+  let gradient = LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.9449525476, green: 0.2824410498, blue: 0.4755416512, alpha: 1)), Color(#colorLiteral(red: 0.7886763215, green: 0.1523601711, blue: 0.6740109324, alpha: 1))]), startPoint: .top, endPoint: .bottom)
+  
   var body: some View {
     NavigationView {
       ZStack {
-        LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.9449525476, green: 0.2824410498, blue: 0.4755416512, alpha: 1)), Color(#colorLiteral(red: 0.7886763215, green: 0.1523601711, blue: 0.6740109324, alpha: 1))]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.bottom)
-
-        WeatherSummaryView()
+        gradient
+          .edgesIgnoringSafeArea(.bottom)
+        
+        VStack {
+          WeatherSummaryView()
+            .padding(
+              EdgeInsets(
+                top: 16,
+                leading: 0,
+                bottom: 24,
+                trailing: 0
+              )
+            )
+        }
       }
-
-        .navigationBarTitle(Text("Weather App"), displayMode: .inline)
-        .navigationBarTitleDisplayMode(.inline)
+      
+      .navigationBarTitle(Text("Weather App"), displayMode: .inline)
+      .navigationBarTitleDisplayMode(.inline)
     }
   }
 }
--- swiftUI-weather-app-demo/WeatherSummaryView.swift
+++ swiftUI-weather-app-demo/WeatherSummaryView.swift
@@ -10,21 +10,44 @@ import SwiftUI
 struct WeatherSummaryView: View {
   @State private var selectedIndex = 0
   private let items = ["Day", "Week", "Month"]
-
+  
   init() {
     UISegmentedControl.appearance().selectedSegmentTintColor = #colorLiteral(red: 0.9449525476, green: 0.2824411094, blue: 0.4755416512, alpha: 1)
     UISegmentedControl.appearance().setTitleTextAttributes([.font: UIFont(name: "pilgi", size: 24)!, .foregroundColor: UIColor.white], for: .selected)
     UISegmentedControl.appearance().setTitleTextAttributes([.font: UIFont(name: "pilgi", size: 24)!, .foregroundColor: #colorLiteral(red: 0.9956122041, green: 0.299066782, blue: 0.5072486997, alpha: 1)], for: .normal)
   }
-
+  
   var body: some View {
     VStack(alignment: .leading) {
+      // segment control section
       Picker(selection: $selectedIndex, label: Text("")) {
         ForEach(0..<items.count) {
           Text(items[$0]).font(.title).tag($0)
         }
       }
       .pickerStyle(SegmentedPickerStyle())
+      .padding(
+        EdgeInsets(
+          top: 24,
+          leading: 16,
+          bottom: 24,
+          trailing: 16
+        )
+      )
+      
+      // weather section
+      HStack {
+        GeometryReader { geometry in
+          Image("sun")
+            .resizable()
+            .scaledToFit()
+            .frame(width: geometry.size.width / 2, height: geometry.size.height / 4)
+        }
+        VStack {
+          Text("123")
+          Text("123")
+        }
+      }
     }
     .frame(maxWidth: .infinity)
     .background(.white)
